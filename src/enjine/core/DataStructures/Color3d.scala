d42f6137625ef17773e836739c93ba8ce1c51c40
Freddie <freddie.poser@westminster.org.uk>
2015-05-19 21:03:05 +0100

    Added Color3d to hold LWJGL colours niceley.

---
+++ src/enjine/core/DataStructures/Color3d.scala
@@ -0,0 +1,11 @@
+package enjine.core.DataStructures
+
+/**
+ * Created by Freddie on 19/05/2015.
+ *
+ */
+class Color3d (val r:Double = 0, val g:Double = 0, val b:Double = 0) {}
+
+object Color3d {
+  val RED = new Color3d(1,0,0)
+}
--- src/enjine/core/Settings/RenderSettings.scala
+++ src/enjine/core/Settings/RenderSettings.scala
@@ -2,8 +2,10 @@ package enjine.core.Settings
 
 /**
  * Created by Freddie on 19/05/2015.
+ *
  */
 class RenderSettings {
+
 
 
 
